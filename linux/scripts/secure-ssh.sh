0996409e5dc92771f1c612aa20c26238dcb3d810
wetzel7 <loganpsavage15@gmail.com>
2026-02-18 22:01:20 -0500

    Fix user creation and update SSH setup script
    Updated user creation command and SSH directory permissions. Commented out root SSH block.

--- linux/scripts/secure-ssh.sh
+++ linux/scripts/secure-ssh.sh
@@ -6,7 +6,6 @@
 
 #!/bin/bash
 
-
 # username provided check
 if [ -z "$1" ]; then
     echo "Usage: $0 <username>"
@@ -16,24 +15,24 @@ fi
 USERNAME=$1
 
 # create user with home dir
-sudo useradd -m -s /bin/bash "$USERNAME"
+sudo useradd -m -s /home/$USERNAME /bin/bash "$USERNAME"
 
-# make ssh dir and set perms
-sudo mkdir -p /home/$USERNAME/.ssh
-sudo chmod 700 /home/$USERNAME/.ssh
+# make ssh dir
+sudo mkdir /home/$USERNAME/.ssh
 
 # copy key
 sudo cp $HOME/SYS-265-Logan-S/linux/public-keys/id_rsa.pub /home/$USERNAME/.ssh/authorized_keys
 
-# set perms
+#set perms
+sudo chmod 700 /home/$USERNAME/.ssh
 sudo chmod 600 /home/$USERNAME/.ssh/authorized_keys && sudo chown -R $USERNAME:$USERNAME /home/$USERNAME/.ssh
 
 # Block Root SSH
-if grep -q "^PermitRootLogin" /etc/ssh/sshd_config; then
-   sudo sed -i 's/^PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
-else
-   echo "PremitRootLogin not found in /etc/ssh/sshd_config"
-fi
+#if grep -q "^PermitRootLogin" /etc/ssh/sshd_config; then
+#   sudo sed -i 's/^PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
+#else
+#   echo "PremitRootLogin not found in /etc/ssh/sshd_config"
+#fi
 
 # restart ssh
 sudo systemctl restart ssh
